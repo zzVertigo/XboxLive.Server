4e8d5b27edf562c037f80604debeeb4ba22c33fa
Pat Hartl <pat@pathar.tl>
2020-03-28 22:01:38 -0500

    Added initial support for dependency injection.
    Configured XServer minimally to work with dependency injection and register it as a service. Refactored configuration loading to work using .NET standards.

--- XboxLive.MACS/Program.cs
+++ XboxLive.MACS/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
@@ -14,22 +17,29 @@ namespace XboxLive.MACS
     public class Program
     {
         public static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        public static IConfigurationRoot Configuration;
 
         private static void Main(string[] args)
         {
+            // Init service collection
+            var services = new ServiceCollection();
+
+            // Configure services (setup DI)
+            ConfigureServices(services);
+
+            // Build the service provider
+            var serviceProvider = services.BuildServiceProvider();
+
             Stopwatch watch = new Stopwatch();
 
             watch.Start();
-
-            var staticData = new StaticData();
-
-            staticData.LoadConfig();
 
             Console.Title = "Xbox Live MACS Server";
 
             Resources.Start();
 
-            XServer server = new XServer(staticData.ServerOptions);
+            var server = serviceProvider.GetService<XServer>();
+
             server.Start();
 
             watch.Stop();
@@ -37,6 +47,18 @@ namespace XboxLive.MACS
             Logger.Info($"Server started in {watch.Elapsed.TotalMilliseconds} ms");
 
             Console.ReadKey();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            services.Configure<ServerOptions>(Configuration.GetSection("ServerOptions"));
+            services.AddSingleton<IConfigurationRoot>(Configuration);
+            services.AddSingleton<XServer>();
         }
     }
 }
